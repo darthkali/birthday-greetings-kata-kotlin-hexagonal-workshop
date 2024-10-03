705939e774031104792c3cae3f66bf80a298aeb0
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-10-03 14:11:37 +0200

    updated Main

--- src/main/java/it/xpug/kata/birthday_greetings/Main.kt
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.kt
@@ -3,8 +3,8 @@ package it.xpug.kata.birthday_greetings
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val service = BirthdayService()
+        val birthdayService = BirthdayService()
         val date = args.get(0)
-        service.sendGreetings("employee_data.txt", XDate(date), "localhost", 25)
+        birthdayService.sendGreetings("employee_data.txt", XDate(date), "localhost", 25)
     }
 }
