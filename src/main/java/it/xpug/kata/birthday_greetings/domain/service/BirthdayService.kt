16a18c8955ad603ce6f476ee31442ab7d8b3ca77
Danny Steinbrecher <Danny.Steinbrecher@ruv.de>
2024-10-11 13:07:17 +0200

    added inbound port

--- src/main/java/it/xpug/kata/birthday_greetings/domain/service/BirthdayService.kt
+++ src/main/java/it/xpug/kata/birthday_greetings/domain/service/BirthdayService.kt
@@ -1,14 +1,15 @@
 package it.xpug.kata.birthday_greetings.domain.service
 
 import it.xpug.kata.birthday_greetings.domain.model.XDate
+import it.xpug.kata.birthday_greetings.domain.port.inbound.ForGreetingEmployee
 import it.xpug.kata.birthday_greetings.domain.port.outbound.ForLoadingEmployees
 import it.xpug.kata.birthday_greetings.domain.port.outbound.ForSendingEmail
 
 class BirthdayService(
     private val forLoadingEmployees: ForLoadingEmployees,
     private val forSendingEmail: ForSendingEmail
-) {
-    fun sendGreetings(xDate: XDate) {
+): ForGreetingEmployee {
+    override fun sendGreetings(xDate: XDate) {
         val employeeList = forLoadingEmployees.loadEmployees()
 
         employeeList.forEach { employee ->
