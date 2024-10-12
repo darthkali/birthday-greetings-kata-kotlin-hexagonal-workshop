981ddff02f4c44e8257e9ce08c73e75ec88e0b1b
dannysteinbrecher <danny.steinbrecher@outlook.com>
2024-10-12 17:31:02 +0200

    removed adapter

--- src/main/java/it/xpug/kata/birthday_greetings/Main.kt
+++ src/main/java/it/xpug/kata/birthday_greetings/Main.kt
@@ -1,7 +1,7 @@
 package it.xpug.kata.birthday_greetings
 
-import it.xpug.kata.birthday_greetings.adapter.inbound.GreetingsCLIAdapter
 import it.xpug.kata.birthday_greetings.adapter.outbound.EmployeeFileReaderAdapter
+import it.xpug.kata.birthday_greetings.domain.model.XDate
 import it.xpug.kata.birthday_greetings.domain.service.BirthdayService
 
 object Main {
@@ -9,7 +9,7 @@ object Main {
     fun main(args: Array<String>) {
         val employeeFileReaderAdapter = EmployeeFileReaderAdapter("employee_data.txt")
         val birthdayService = BirthdayService(employeeFileReaderAdapter)
-        val greetingsCLIAdapter = GreetingsCLIAdapter(birthdayService)
-        greetingsCLIAdapter.sendGreetings(args, "localhost", 25)
+        val date = args.get(0)
+        birthdayService.sendGreetings(XDate(date), "localhost", 25)
     }
 }
